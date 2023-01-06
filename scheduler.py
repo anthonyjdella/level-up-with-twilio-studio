726045fed9e142730c94b7b312089ea3e3459ffd
Anthony_Dellavecchia <dev@anthonydellavecchia.com>
2023-01-06 09:43:36 -0800

    refactor(modular): wrap code into functions

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,13 @@
 import os
 from datetime import datetime
 from datetime import timedelta
+
 from twilio.rest import Client
 from dotenv import load_dotenv
 
+
 load_dotenv()
+
 
 account_sid = os.getenv('TWILIO_ACCOUNT_SID')
 auth_token = os.getenv('TWILIO_AUTH_TOKEN')
@@ -14,25 +17,30 @@ client = Client(account_sid, auth_token)
 # Check the current time in UTC
 print(datetime.utcnow())
 
-# Schedule a message to be sent in between 15 minutes (901 seconds) to 7 days in the future
-fifteen_minutes_from_now = datetime.utcnow() + timedelta(seconds=901)
-print(fifteen_minutes_from_now)
+# Scheduled message must be be sent in between 15 minutes (901 seconds) to 7 days in the future
+sixteen_minutes_from_now = datetime.utcnow() + timedelta(minutes=16)
+print(sixteen_minutes_from_now)
 
-# Schedule a message to be sent at a specific (fixed) time
-message = client.messages \
-    .create(
-        messaging_service_sid=os.getenv('TWILIO_MSG_SRVC_SID'),
-        to=os.getenv('MY_PERSONAL_NUMBER'),
-        body='This is a scheduled message',
-        send_at=fifteen_minutes_from_now,
-        schedule_type='fixed'
-    )
-print(message.sid)
+
+# Schedule a message to be sent at a fixed time
+def schedule_message(body):
+    message = client.messages \
+        .create(
+            messaging_service_sid=os.getenv('TWILIO_MSG_SRVC_SID'),
+            to=os.getenv('MY_PERSONAL_NUMBER'),
+            body=body,
+            send_at=sixteen_minutes_from_now,
+            schedule_type='fixed'
+        )
+    print(message.sid)
+
 
 # Cancel a scheduled message
-'''
-message = client.messages('SMa3e29bf635f9b0eb8edda580c6feeeaa') \
+def cancel_message(message_sid):
+    message = client.messages(message_sid) \
     .update(status='canceled')
 
-print(message.status)
-'''
+    print(message.status)
+
+
+schedule_message('This will be sent in 15 minutes')
